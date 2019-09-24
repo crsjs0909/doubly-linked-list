f568bad27877738abcbd48df308968a81332cce3
oleg kin <noklige@gmail.com>
2019-09-24 19:38:19 +0300

    feat: class implemented

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,115 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
 
-    append(data) {}
+    constructor() {
+        this.hd = new Node(null, null, null);
+        this.hd.prev = this.hd;
+        this.hd.next = this.hd;
+        this.length = 0;
+        this.syncHd();
+    }
 
-    head() {}
+    append(data) {
+        let node = new Node(data, this.hd.prev, this.hd);
+        this.hd.prev.next = node;
+        this.hd.prev = node;
+        this.length++;
+        this.syncHd();
+        return this;
+    }
 
-    tail() {}
+    head() {
+        return this.hd.next.data;
+    }
 
-    at(index) {}
+    tail() {
+        return this.hd.prev.data;
+    }
 
-    insertAt(index, data) {}
+    at(index) {
+        return this.nodeAt(index).data;
+    }
 
-    isEmpty() {}
+    nodeAt(index) {
+        if (index >= this.length || index < 0)
+            throw "IndexOutOfBoundException: " + index;
+        let ptr = this.hd.next;
+        for (let i = 0; i < index; i++) {
+            ptr = ptr.next;
+        }
+        return ptr;
+    }
 
-    clear() {}
+    syncHd() {
+        this._head = this.hd.next;
+        this._tail = this.hd.prev;
+    }
 
-    deleteAt(index) {}
+    insertAt(index, data) {
+        if (index < 0 || index > this.length)
+            throw "IndexOutOfBoundException: " + index;
+        if (index === this.length)
+            return this.append(data);
+        else {
+            let nextNode = this.nodeAt(index);
+            let prevNode = nextNode.prev;
+            let currNude = new Node(data, prevNode, nextNode);
+            prevNode.next = currNude;
+            nextNode.prev = currNude;
+            this.length++;
+        }
+        this.syncHd();
+        return this;
+    }
 
-    reverse() {}
+    isEmpty() {
+        return this.length === 0;
+    }
 
-    indexOf(data) {}
+    clear() {
+        this.hd.prev = this.hd;
+        this.hd.next = this.hd;
+        this.length = 0;
+        this.syncHd();
+        return this;
+    }
+
+    deleteAt(index) {
+        if (index < 0 || index >= this.length)
+            throw "IndexOutOfBoundException: " + index;
+        let currNode = this.nodeAt(index);
+        let prevNode = currNode.prev;
+        let nextNode = currNode.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        this.length--;
+        this.syncHd();
+        return this;
+    }
+
+    reverse() {
+        let node = this.hd;
+        for (let i = 0; i < this.length; i++) {
+            node = node.next;
+            let tmp = node.next;
+            node.next = node.prev;
+            node.prev = tmp;
+        }
+        this.syncHd();
+        return this;
+    }
+
+    indexOf(data) {
+        let node = this.hd;
+        for (let i = 0; i < this.length; i++) {
+            node = node.next;
+            if (node.data === data) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
